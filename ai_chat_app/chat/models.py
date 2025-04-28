b4ba4dad6e98bbaacb05f4e6cfb1b79eef2df54e
Anton Agibalov <antoshka_agibalov@mail.ru>
2025-04-28 16:15:03 +0300

    chore: улучшил модели чата

--- ai_chat_app/chat/models.py
+++ ai_chat_app/chat/models.py
@@ -3,18 +3,26 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Chat(models.Model):
-    id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=100, default="Новый чат")
+class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        abstract = True
+
+
+class Chat(BaseModel):
+    id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='chats'
+    )
+    title = models.CharField(max_length=100, default="Новый чат")
     modified_at = models.DateTimeField(auto_now=True)
 
 
-class Message(models.Model):
+class Message(BaseModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     chat = models.ForeignKey(
         Chat, on_delete=models.CASCADE, related_name="messages"
     )
     text = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
