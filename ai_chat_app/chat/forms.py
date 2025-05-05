870fb289954802fd7923c9e7dab1e0fa67a74e41
Anton Agibalov <antoshka_agibalov@mail.ru>
2025-05-05 15:30:16 +0300

    fix: переделал форму для отправки сообщения

--- ai_chat_app/chat/forms.py
+++ ai_chat_app/chat/forms.py
@@ -1,15 +1,22 @@
 from django import forms
+from . import models
 
 
-class CreateMessageForm(forms.Form):
-    text = forms.CharField(
-        label='',
-        widget=forms.Textarea(
-            attrs={
-                'class': 'form-control shadow-none border-0 bg-transparent p-0 overflow-auto',
-                'placeholder': 'Введите текст...',
-                'style': 'resize: none;',
-                'rows': 3
-            }
+class MessageForm(forms.ModelForm):
+    class Meta:
+        model = models.Message
+        fields = ['text']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['text'] = forms.CharField(
+            label='',
+            widget=forms.Textarea(
+                attrs={
+                    'class': 'form-control shadow-none border-0 bg-transparent p-0 overflow-auto',
+                    'placeholder': 'Введите текст...',
+                    'style': 'resize: none;',
+                    'rows': 3
+                }
+            )
         )
-    )
