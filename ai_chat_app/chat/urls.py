9fec7e79086ebe58cb1d15c266d047689ecfd156
Anton Agibalov <antoshka_agibalov@mail.ru>
2025-05-05 15:29:31 +0300

    feat: сделал получение пользователя из сессии

--- ai_chat_app/chat/urls.py
+++ ai_chat_app/chat/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('start/', views.start_new_chat),
     path('<str:chat_uuid>/', views.chat),
+    path('', views.start_page),
 ]
--- ai_chat_app/chat/views.py
+++ ai_chat_app/chat/views.py
@@ -1,32 +1,48 @@
 from django.shortcuts import redirect, render, get_object_or_404
+from django.contrib.auth import get_user
 from . import models
 from . import forms
 
 
 def chat(request, chat_uuid):
     chat = get_object_or_404(models.Chat, pk=chat_uuid)
-    mock_user = models.User.objects.get(pk=1)
+    user = get_user(request)
 
     if request.method == 'POST':
-        form = forms.CreateMessageForm(request.POST)
+        form = forms.MessageForm(request.POST)
 
         if form.is_valid():
-            text = form.cleaned_data['text']
-            message = models.Message(chat=chat, user=mock_user, text=text)
+            message = form.save(commit=False)
+            message.user = user
+            message.chat = chat
             message.save()
 
     context = {
         'title': chat.title,
         'messages': chat.messages.all(),
-        'chats': mock_user.chats.all(),
+        'chats': user.chats.all(),
         'current_chat_id': chat.id,
-        'form': forms.CreateMessageForm()
+        'form': forms.MessageForm(initial={
+            'user': user,
+            'chat': chat.id,
+        })
     }
 
-    return render(request, 'chat/chat.html', context)
+    return render(request, 'chat/conversation.html', context)
 
 
 def start_new_chat(request):
     mock_user = models.User.objects.get(pk=1)
     chat = models.Chat.objects.create(user=mock_user)
     return redirect(f'/chat/{chat.id}')
+
+
+def start_page(request):
+    user = get_user(request)
+
+    context = {
+        'chats': user.chats.all(),
+        'form': forms.MessageForm()
+    }
+
+    return render(request, 'chat/start.html', context)
